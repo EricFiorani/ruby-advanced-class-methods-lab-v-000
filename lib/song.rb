d07d30b3313be2eaf3887947b6a37ce5396dc7ed
Eric Fiorani <efiorani305@gmail.com>
2018-05-08 19:48:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,14 @@ class Song
 
   def self.create
     song = self.new
-    song.name = name
-    @@all << song
+    song.save
     song
   end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
 
   def self.all
     @@all
